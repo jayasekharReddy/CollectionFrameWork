b1bc55c6bce1cf7823e356e9633c96bd68c85e36
jayasekharreddy <jayasekharreddy13@gmail.com>
2023-08-18 11:04:21 +0530

    intialized a7

--- src/swappingInJava/Swapping2Numbers.java
+++ src/swappingInJava/Swapping2Numbers.java
@@ -5,14 +5,14 @@ public class Swapping2Numbers {
     public static void main(String[] args){
 
         int a = 10, b=20;
-
+int a7;
         System.out.println("Before swapping values " + a + "  " + b);
 
 //        int t = a;
 //        a=b;
 //        b=t;
 
-        a = a+b;
+         a7 = a + b;
         b = a-b;
         a = a-b;
         System.out.println(" After Before swapping values " + a + "  " + b);
